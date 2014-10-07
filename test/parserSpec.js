ccc881ab866fd511f18294c60a143b85ce8b9ad6
Jan Hein Hoogstad <yeehaa@codingthehumanities.com>
2014-10-07 13:24:34 +0200

    refactor tests

--- test/parserSpec.js
+++ test/parserSpec.js
@@ -1,13 +1,7 @@
-
-
-
-
-
-
-
 let expect = require("chai").expect;
 
 class Parser {
+
   constructor(){
     this.parsedObject = {};
   }
@@ -25,11 +19,15 @@ class Parser {
   }
 }
 
-
 context("parser", () => {
 
+  let parser;
+
+  beforeEach(() => {
+    parser = new Parser();
+  })
+
   it('instantiates a new parser', () => {
-    let parser = new Parser();
     expect(parser).to.be.an.instanceof(Parser);
   });
 
@@ -37,27 +35,24 @@ context("parser", () => {
 
     describe("without arguments", ()=>{
       it('returns an empty card object', () => {
-        let parser = new Parser();
         let object = parser.parse();
         expect(object).to.be.an.instanceof(Object);
       });
     });
 
     describe("with a single line argument", () => {
+
       it('returns an object', () => {
-        let parser = new Parser();
         let object = parser.parse('title: Hello World');
         expect(object.title).to.equal("Hello World");
       });
 
       it('correctly parses the input string', () => {
-        let parser = new Parser();
         let object = parser.parse("title: Goodbye World");
         expect(object.title).to.equal("Goodbye World");
       });
 
       it('should erase redundant whitespace', () => {
-        let parser = new Parser();
         let object = parser.parse("title:  Goodbye World");
         expect(object.title).to.equal("Goodbye World");
       });
