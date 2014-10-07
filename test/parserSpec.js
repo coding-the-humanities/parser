d3f46a592c15076dbf758de3241ee618e6c4c992
Jan Hein Hoogstad <yeehaa@codingthehumanities.com>
2014-10-07 12:01:23 +0200

    add extra test

--- test/parserSpec.js
+++ test/parserSpec.js
@@ -1,31 +1,45 @@
-var expect = require("chai").expect;
+let expect = require("chai").expect;
+
+function Parser(){
+}
+
+Parser.prototype.parse = function(){
+  return {
+    title: "Hello World"
+  };
+}
 
 context("parser", () => {
-  it('should be able to instantiate a new parser', () => {
+
+  it('instantiates a new parser', () => {
     let parser = new Parser();
     expect(parser).to.be.an.instanceof(Parser);
   });
 
   context("parse method", ()=>{
 
-    xdescribe("without arguments", ()=>{
-      it('should return an empty card object', () => {
+    describe("without arguments", ()=>{
+      it('returns an empty card object', () => {
         let parser = new Parser();
         let object = parser.parse();
         expect(object).to.be.an.instanceof(Object);
       });
     });
 
-    xdescribe("with a single line argument", () => {
-      it('should return an object', () => {
+    describe("with a single line argument", () => {
+      it('returns an object', () => {
         let parser = new Parser();
         let object = parser.parse('title: Hello World');
         expect(object.title).to.equal("Hello World");
       });
-    })
 
-    xdescribe("with a single line argument", () => {
-      it('should erase redundant whitespace', () => {
+      it('correctly parses the input string', () => {
+        let parser = new Parser();
+        let object = parser.parse("title: Goodbye World");
+        expect(object.title).to.equal("Goodbye World");
+      });
+
+      xit('should erase redundant whitespace', () => {
         let parser = new Parser();
         let object = parser.parse("title:     Goodbye World");
         expect(object.title).to.equal("Goodbye World");
